8a726fb62859a104213fa1f100e177f28e17a193
wendeng <w526529650@outlook.com>
2019-08-21 23:08:58 +0800

    Initial Commit

---
+++ pages/history/history.js
@@ -0,0 +1,29 @@
+// pages/history/history.js
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    history: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  onShow: function () {
+    this.setData({ history: wx.getStorageSync('history') })
+  },
+
+  onTapItem: function (e) {
+    wx.reLaunch({
+      url: `/pages/index/index?query=${e.currentTarget.dataset.query}`
+    })
+  }
+})
